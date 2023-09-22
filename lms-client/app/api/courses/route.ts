21119ba867e4dbe9f2aa77f4d500fc19d13f1438
Steven <digitalexile99@gmail.com>
2023-09-21 20:58:33 -0600

    added image uploading via uploadthing

--- lms-client/app/api/courses/route.ts
+++ lms-client/app/api/courses/route.ts
@@ -2,6 +2,7 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
+// example of post request
 export async function POST(req: Request) {
   try {
     const { userId } = auth();
